05f72e5f1f5445a44440fb14277fa1c2b28d290b
Maaz Mushtaq <mmushtaq46@gmail.com>
2020-12-31 01:12:27 +0500

    appcompleted

--- application/controllers/api/WaterApi.php
+++ application/controllers/api/WaterApi.php
@@ -8,10 +8,11 @@ class WaterApi extends REST_Controller{
         $this->load->model('WaterModel', 'wm');
     }
 
-    public function check_get(){
+    /* public function check_get(){
         $this->response($this->input->get(), 200);
-    }
+    } */
 
+    //REGISTERING CUSTOMER
     public function register_post(){
         $email = $this->input->get('email');
         $emailverify = $this->wm->check_email($email);
@@ -40,6 +41,7 @@ class WaterApi extends REST_Controller{
         endif;
     }
 
+    //CUSTOMER LOGIN
     public function login_post(){
        $email = $this->input->get('email');
        $password = $this->input->get('password');
@@ -57,18 +59,141 @@ class WaterApi extends REST_Controller{
        endif;
     }
 
+
+    //FETCH ALL PRODUCTS
     public function fetch_products_get(){
         if(!empty($this->wm->fetch_products())):
             $this->response($this->wm->fetch_products(),200);
         endif;
     }
 
+
+    //POSTING ORDERS
     public function order_post(){
-        if($this->wm->order($this->input->get())):
-            $this->response([
-                'message'=>'Ordered Successfully'
-            ]);
+        
+        //$this->response(json_decode($this->input->get('myarray')));
+        //$this->response($this->input->get());exit;
+        if($this->input->get('payment_type') == 'EasyPaisa'):
+                if(!empty($this->input->get('easyimge'))):
+                        $data['ordered_by'] = $this->input->get('ordered_by');
+                        $data['grandTotal'] = $this->input->get('grandTotal');
+                        $data['payment_type'] = $this->input->get('payment_type');
+                        $data['image'] = $this->input->get('easyimge');
+                        $data['myarray'] = json_decode($this->input->get('myarray'), true);
+                        //$this->response($data);exit;
+                        //$data['easyimage'] = $this->input->get('easyimge');
+                        //$this->response($this->wm->order($data));exit;
+                        if($this->wm->order($data)):
+                            $this->response([
+                                'message'=>'Ordered Successfully'
+                            ]);
+                            
+                            else:
+                                $this->response([
+                                    'message'=>'Problem Inserting Order'
+                                    ]);
+                        endif;
+                else:
+                    $this->response(['message'=>'Select A Screenshot']);
+                endif;
+            else:
+                        $data['ordered_by'] = $this->input->get('ordered_by');
+                        $data['grandTotal'] = $this->input->get('grandTotal');
+                        $data['payment_type'] = $this->input->get('payment_type');
+                        //$data['image'] = $this->input->get('easyimge');
+                        $data['myarray'] = json_decode($this->input->get('myarray'), true);
+                        //$this->response($data);exit;
+                        //$data['easyimage'] = $this->input->get('easyimge');
+                        //$this->response($this->wm->order($data));exit;
+                        if($this->wm->order_cod($data)):
+                            $this->response([
+                                'message'=>'Ordered Successfully'
+                            ]);
+                            
+                            else:
+                                $this->response([
+                                    'message'=>'Problem Inserting Order'
+                                    ]);
+                        endif;
         endif;
+    }
+    
+    
+    //FETCHING PENDING ORDERS
+    public function fetch_pending_orders_get(){
+       // if(!empty($this->wm->fetch_pending_orders())):
+            $this->response($this->wm->fetch_pending_orders());
+        //endif;
+    }
+    
+
+    //FETCH PENDING ORDER DETAILS
+    public function fetch_pending_order_details_get(){
+        $order_id = $this->input->get('order_id');
+        $this->response($this->wm->fetch_pending_orders_details($order_id));
+    }
+    
+
+    //MARK AS COMPLETED
+    public function mark_completed_post(){
+        $order_id = $this->input->get('order_id');
+        if($this->wm->mark_completed($order_id)):
+            $this->response([
+                'message'=>'Delivered Successfully'
+                ]);
+                
+                else:
+                    $this->response([
+                'message'=>'Some Error Occured'
+                ]);
+        endif;
+    }
+    
+    //FETCH DELIVERED ORDERS
+    public function fetch_delivered_orders_get(){
+       // if(!empty($this->wm->fetch_pending_orders())):
+            $this->response($this->wm->fetch_delivered_orders());
+        //endif;
+    }
+    
+    public function fetch_delivered_order_details_get(){
+        $order_id = $this->input->get('order_id');
+        $this->response($this->wm->fetch_delivered_orders_details($order_id));
+    }
+    
+    
+    public function login_driver_post(){
+       $email = $this->input->get('email');
+       $password = $this->input->get('password');
+        
+       if(!empty($this->wm->login_driver($email, $password))):
+        $this->response([
+            'message'=>'Login Successfull',
+            'data'=>$this->wm->login_driver($email, $password)
+        ]);
+
+        else:
+            $this->response([
+                'message'=>'Incorrect Email / Password'
+            ]); 
+       endif;
+    }
+    
+    public function count_all_get(){
+        $count['delivered_count'] = $this->wm->count_delivered_orders();
+        $count['pending_count'] = $this->wm->count_pending_orders();
+        $this->response($count);
+    }
+    
+    public function fetch_specific_user_orders_get(){
+        $userid = $this->input->get('user_id');
+        $this->response($this->wm->fetch_specific_user_orders($userid));
+    }
+    
+    
+    public function fetch_specific_user_order_details_get(){
+        $order_id = $this->input->get('order_id');
+        $this->response($this->wm->fetch_specific_user_orders_details($order_id));
     }
 
 }
--- application/models/WaterModel.php
+++ application/models/WaterModel.php
@@ -16,6 +16,10 @@ class WaterModel extends CI_Model{
     public function login($email, $password){
         return $this->db->from('customers')->where('email',$email)->where('password',$password)->get()->row_array();
     }
+    
+    public function login_driver($email, $password){
+        return $this->db->from('driver')->where('driver_email',$email)->where('driver_password',$password)->get()->row_array();
+    }
 
     public function fetch_products(){
         return $this->db->from('products')->get()->result_array();
@@ -26,25 +30,123 @@ class WaterModel extends CI_Model{
         $myarray['ordered_by'] = $mydata['ordered_by'];
         $myarray['grandTotal'] = $mydata['grandTotal'];
         $myarray['payment_type'] = $mydata['payment_type'];
-        
+        $myarray['easyimage'] = $mydata['image'];
         $this->db->insert('orders',$myarray);
         $last_insert_id = $this->db->insert_id();
-
         $order_items = array();
         /* echo '<pre>';
         print_r($mydata['myarray']);
         echo '</pre>'; */
-
-        for($i=0; $i<count($mydata['myarray']); $i++):
-            $order_items[$i]['product_id'] = $mydata['myarray'][$i]['product_id'];
+        for($i=0; $i<count($mydata['myarray'][0]); $i++):
+            if($mydata['myarray'][0][$i]['quantity'] != 0):
+                $order_items[$i]['product_id'] = $mydata['myarray'][0][$i]['product_id'];
             $order_items[$i]['order_id'] = $last_insert_id;
-            $order_items[$i]['quantity'] = $mydata['myarray'][$i]['quantity'];
-            $order_items[$i]['sub_total'] = $mydata['myarray'][$i]['subtotal'];
+            $order_items[$i]['quantity'] = $mydata['myarray'][0][$i]['quantity'];
+            $order_items[$i]['sub_total'] = $mydata['myarray'][0][$i]['subtotal'];
+            endif;
         endfor;
-
         return $this->db->insert_batch('order_items', $order_items);
 
     }
+    
+    public function order_cod($mydata){
+        $myarray = array();
+        $myarray['ordered_by'] = $mydata['ordered_by'];
+        $myarray['grandTotal'] = $mydata['grandTotal'];
+        $myarray['payment_type'] = $mydata['payment_type'];
+        $this->db->insert('orders',$myarray);
+        $last_insert_id = $this->db->insert_id();
+        $order_items = array();
+        /* echo '<pre>';
+        print_r($mydata['myarray']);
+        echo '</pre>'; */
+        for($i=0; $i<count($mydata['myarray'][0]); $i++):
+            if($mydata['myarray'][0][$i]['quantity'] != 0):
+                $order_items[$i]['product_id'] = $mydata['myarray'][0][$i]['product_id'];
+            $order_items[$i]['order_id'] = $last_insert_id;
+            $order_items[$i]['quantity'] = $mydata['myarray'][0][$i]['quantity'];
+            $order_items[$i]['sub_total'] = $mydata['myarray'][0][$i]['subtotal'];
+            endif;
+        endfor;
+        return $this->db->insert_batch('order_items', $order_items);
+
+    }
+    
+    public function fetch_pending_orders(){
+        return $this->db
+        ->select('orders.order_id, cs.full_name, orders.grandTotal, orders.ordered_at, orders.payment_type,cs.address, cs.email')
+        ->from('orders')
+        ->join('customers as cs','orders.ordered_by = cs.id')
+        ->where('orders.order_status','pending')
+        ->get()
+        ->result_array();
+    }
+    
+    public function fetch_pending_orders_details($order_id){
+        return $this->db
+        ->select('products.size, order_items.quantity, products.product_price, order_items.sub_total')
+        ->from('order_items')
+        ->join('products','products.id = order_items.product_id')
+        ->where('order_items.order_id', $order_id)
+        ->get()
+        ->result_array();
+    }
+    
+    public function mark_completed($order_id){
+        return $this->db
+        ->set('order_status','delivered')
+        ->where('order_id',$order_id)
+        ->update('orders');
+    }
+    
+    
+    public function fetch_delivered_orders(){
+        return $this->db
+        ->select('orders.order_id, cs.full_name, orders.grandTotal, orders.ordered_at, orders.payment_type,cs.address, cs.email')
+        ->from('orders')
+        ->join('customers as cs','orders.ordered_by = cs.id')
+        ->where('orders.order_status','delivered')
+        ->get()
+        ->result_array();
+    }
+    
+    public function fetch_delivered_details($order_id){
+        return $this->db
+        ->select('products.size, order_items.quantity, products.product_price, order_items.sub_total')
+        ->from('order_items')
+        ->join('products','products.id = order_items.product_id')
+        ->where('order_items.order_id', $order_id)
+        ->get()
+        ->result_array();
+    }
+    
+    public function count_delivered_orders(){
+        return $this->db->from('orders')->where('order_status','delivered')->count_all_results();
+    }
+    
+    public function count_pending_orders(){
+        return $this->db->from('orders')->where('order_status','pending')->count_all_results();
+    }
+    
+    public function fetch_specific_user_orders($userid){
+        return $this->db
+        ->select('orders.order_id, orders.grandTotal, orders.ordered_at, orders.payment_type, orders.order_status')
+        ->from('orders')
+        ->where('orders.ordered_by',$userid)
+        ->get()
+        ->result_array();
+    }
+    
+    public function fetch_specific_user_order_details($order_id){
+        return $this->db
+        ->select('products.size, order_items.quantity, products.product_price, order_items.sub_total')
+        ->from('order_items')
+        ->join('products','products.id = order_items.product_id')
+        ->where('order_items.order_id', $order_id)
+        ->get()
+        ->result_array();
+    }
+    
 }
 
 
